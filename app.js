2f0d3394f32a540fe86ee9cef5e5e4f6714ea698
Patrick <patrickwestervelt.us@gmail.com>
2019-02-16 18:05:17 -0500

    Added User Authentication

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require("express"),
 	mongoose = require("mongoose"),
 	bodyParser = require("body-parser"),
 	Film = require("./models/film"),
-	Comment = require("./models/comment")
+	Comment = require("./models/comment"),
+	passport = require("passport"),
+	LocalStrategy = require("passport-local"),
+	User = require("./models/user")
 
 //Link to my database
 mongoose.connect("mongodb://Patrick:Password@cluster0-shard-00-00-l1try.mongodb.net:27017,cluster0-shard-00-01-l1try.mongodb.net:27017,cluster0-shard-00-02-l1try.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true")
@@ -16,8 +19,26 @@ app.use(express.static(__dirname + '/public'));
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "I own a pair of orange Heelys and I love them.",
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 //Sets ejs as default view engine
 app.set("view engine","ejs")
+
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user
+	next()
+})
 
 //Loads index page, passes in film data
 app.get("/",function(req,res){
@@ -52,7 +73,7 @@ app.get("/films/:id/comments",function(req,res){
 	})
 })
 
-app.post("/films/:id/comments",function(req,res){
+app.post("/films/:id/comments",isLoggedIn,function(req,res){
 	Film.findById(req.params.id,function(err,film){
 		if(err){
 			console.log(err)
@@ -71,6 +92,48 @@ app.post("/films/:id/comments",function(req,res){
 		}
 	})
 })
+
+//AUTHORIZATION ROUTES
+
+app.get("/register",function(req,res){
+	res.render("register")
+})
+
+app.post("/register",function(req,res){
+	var newUser = new User({username: req.body.username})
+	User.register(newUser, req.body.password,function(err,user){
+		if(err){
+			console.log(err)
+			return res.render("register")
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/")
+		})
+	})
+})
+
+app.get("/login",function(req,res){
+	res.render("login")
+})
+
+app.post("/login",passport.authenticate("local",
+	{successRedirect:"/",
+	failureRedirect:"/login"
+	}),function(req,res){
+})
+
+app.get("/logout",function(req,res){
+	req.logout()
+	res.redirect("/")
+})
+
+//MIDDLEWARE
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login")
+}
 
 //Gets the server running
 app.listen(port, hostname, function(){
